ced1f635cf24147bae169d28bc7d817c7c8bd3b7
Ramprasad Chauhan <ramprasadchauhan7@gmail.com>
2023-12-11 20:49:04 +0530

    Nth Chat

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
 import { useSearchParams } from "react-router-dom";
+import CommentContainer from "./CommentContainer";
 
 const WatchPage = () => {
   const [seachParams] = useSearchParams();
@@ -14,14 +15,15 @@ const WatchPage = () => {
   return (
     <div className="ml-1">
       <iframe
-        width="800"
-        height="420"
+        width="700"
+        height="360"
         src={"https://www.youtube.com/embed/" + para}
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
       ></iframe>
+      <CommentContainer />
     </div>
   );
 };
